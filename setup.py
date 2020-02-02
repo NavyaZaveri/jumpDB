dfe2ca5659348ae8cc00507b5b560c5c9236de45
NavyaZaveri <navyajayesh+1@gmail.com>
2020-02-02 13:48:17 +0000

    update setup.py to incldue reqs

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name="jumpDB",  # Replace with your own username
     version="0.0.1",
@@ -13,6 +16,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/NavyaZaveri/jumpDB",
     packages=setuptools.find_packages(),
+    install_requires=required,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: BSD 2-Clause",
